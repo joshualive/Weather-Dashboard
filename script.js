d1bf969a6b03073df7189021d7b6ba34a83d8c85
Joshua Miller <66288776+joshualive@users.noreply.github.com>
2020-09-20 20:59:27 -0400

    Completed getting 5 day forecast and outputting to html

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 let searchedCities = []
 let cityName = document.getElementById('cityName')
-let currentTemp = document.getElementById('currentTemp')
-let currentHumidity = document.getElementById('currentHumidity')
-let currentWind = document.getElementById('currentWind')
-let currentUV = document.getElementById('currentUV')
-
 
 document
     .getElementById('search')
@@ -32,22 +27,72 @@ document
                     .then(function (weatherResults) {
                         console.log(weatherResults)
 
-                        // Get today's weather.
+                        // Today's weather.
                         cityName.innerHTML = `
                         ${city} 
                         <img src="https://openweathermap.org/img/wn/${weatherResults.current.weather[0].icon}.png" alt="${weatherResults.current.weather[0].description} weather icon.">`
 
-                        currentTemp.innerHTML = `
+                        document.getElementById('currentTemp').innerHTML = `
                         <strong>Temperature:</strong> ${weatherResults.current.temp} C`
 
-                        currentHumidity.innerHTML = `
+                        document.getElementById('currentHumidity').innerHTML = `
                         <strong>Humidity:</strong> ${weatherResults.current.humidity}%`
 
-                        currentWind.innerHTML = `
-                        <strong>Wind Speed:</strong> ${weatherResults.current.wind_speed}`
+                        document.getElementById('currentWind').innerHTML = `
+                        <strong>Wind Speed:</strong> ${weatherResults.current.wind_speed} KMPH`
 
-                        currentUV.innerHTML = `
+                        document.getElementById('currentUV').innerHTML = `
                         <strong>UV Index:</strong> ${weatherResults.current.uvi}`
+
+                        //Tomorrow's weather
+                        document.getElementById('tomorrowIcon').innerHTML = `
+                        <img src="https://openweathermap.org/img/wn/${weatherResults.daily[0].weather[0].icon}.png" alt="${weatherResults.daily[0].weather[0].description} weather icon.">`
+
+                        document.getElementById('tomorrowTemp').innerHTML = `
+                        <strong>Temperature:</strong> ${weatherResults.daily[0].temp.day} C`
+
+                        document.getElementById('tomorrowHumidity').innerHTML = `
+                        <strong>Humidity:</strong> ${weatherResults.daily[0].humidity}%`
+
+                        // Day 2's weather
+                        document.getElementById('dayTwoIcon').innerHTML = `
+                        <img src="https://openweathermap.org/img/wn/${weatherResults.daily[1].weather[0].icon}.png" alt="${weatherResults.daily[1].weather[0].description} weather icon.">`
+
+                        document.getElementById('dayTwoTemp').innerHTML = `
+                        <strong>Temperature:</strong> ${weatherResults.daily[1].temp.day} C`
+
+                        document.getElementById('dayTwoHumidity').innerHTML = `
+                        <strong>Humidity:</strong> ${weatherResults.daily[1].humidity}%`
+
+                        // Day 3's weather
+                        document.getElementById('dayThreeIcon').innerHTML = `
+                        <img src="https://openweathermap.org/img/wn/${weatherResults.daily[2].weather[0].icon}.png" alt="${weatherResults.daily[2].weather[0].description} weather icon.">`
+
+                        document.getElementById('dayThreeTemp').innerHTML = `
+                        <strong>Temperature:</strong> ${weatherResults.daily[2].temp.day} C`
+
+                        document.getElementById('dayThreeHumidity').innerHTML = `
+                        <strong>Humidity:</strong> ${weatherResults.daily[2].humidity}%`
+
+                        // Day 4's weather
+                        document.getElementById('dayFourIcon').innerHTML = `
+                        <img src="https://openweathermap.org/img/wn/${weatherResults.daily[3].weather[0].icon}.png" alt="${weatherResults.daily[3].weather[0].description} weather icon.">`
+
+                        document.getElementById('dayFourTemp').innerHTML = `
+                        <strong>Temperature:</strong> ${weatherResults.daily[3].temp.day} C`
+
+                        document.getElementById('dayFourHumidity').innerHTML = `
+                        <strong>Humidity:</strong> ${weatherResults.daily[3].humidity}%`
+
+                        // Day 5's weather
+                        document.getElementById('dayFiveIcon').innerHTML = `
+                        <img src="https://openweathermap.org/img/wn/${weatherResults.daily[4].weather[0].icon}.png" alt="${weatherResults.daily[4].weather[0].description} weather icon.">`
+
+                        document.getElementById('dayFiveTemp').innerHTML = `
+                        <strong>Temperature:</strong> ${weatherResults.daily[4].temp.day} C`
+
+                        document.getElementById('dayFiveHumidity').innerHTML = `
+                        <strong>Humidity:</strong> ${weatherResults.daily[4].humidity}%`
 
                     })
 
