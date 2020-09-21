3a63e2640b7e83c270200a0d70147d9d8a4eb7c5
Joshua Miller <66288776+joshualive@users.noreply.github.com>
2020-09-20 21:07:50 -0400

    Update colour of 5 day forecast cards

--- script.js
+++ script.js
@@ -49,50 +49,50 @@ document
                         <img src="https://openweathermap.org/img/wn/${weatherResults.daily[0].weather[0].icon}.png" alt="${weatherResults.daily[0].weather[0].description} weather icon.">`
 
                         document.getElementById('tomorrowTemp').innerHTML = `
-                        <strong>Temperature:</strong> ${weatherResults.daily[0].temp.day} C`
+                        Temperature: ${weatherResults.daily[0].temp.day} C`
 
                         document.getElementById('tomorrowHumidity').innerHTML = `
-                        <strong>Humidity:</strong> ${weatherResults.daily[0].humidity}%`
+                        Humidity: ${weatherResults.daily[0].humidity}%`
 
                         // Day 2's weather
                         document.getElementById('dayTwoIcon').innerHTML = `
                         <img src="https://openweathermap.org/img/wn/${weatherResults.daily[1].weather[0].icon}.png" alt="${weatherResults.daily[1].weather[0].description} weather icon.">`
 
                         document.getElementById('dayTwoTemp').innerHTML = `
-                        <strong>Temperature:</strong> ${weatherResults.daily[1].temp.day} C`
+                        Temperature: ${weatherResults.daily[1].temp.day} C`
 
                         document.getElementById('dayTwoHumidity').innerHTML = `
-                        <strong>Humidity:</strong> ${weatherResults.daily[1].humidity}%`
+                        Humidity: ${weatherResults.daily[1].humidity}%`
 
                         // Day 3's weather
                         document.getElementById('dayThreeIcon').innerHTML = `
                         <img src="https://openweathermap.org/img/wn/${weatherResults.daily[2].weather[0].icon}.png" alt="${weatherResults.daily[2].weather[0].description} weather icon.">`
 
                         document.getElementById('dayThreeTemp').innerHTML = `
-                        <strong>Temperature:</strong> ${weatherResults.daily[2].temp.day} C`
+                        Temperature: ${weatherResults.daily[2].temp.day} C`
 
                         document.getElementById('dayThreeHumidity').innerHTML = `
-                        <strong>Humidity:</strong> ${weatherResults.daily[2].humidity}%`
+                        Humidity: ${weatherResults.daily[2].humidity}%`
 
                         // Day 4's weather
                         document.getElementById('dayFourIcon').innerHTML = `
                         <img src="https://openweathermap.org/img/wn/${weatherResults.daily[3].weather[0].icon}.png" alt="${weatherResults.daily[3].weather[0].description} weather icon.">`
 
                         document.getElementById('dayFourTemp').innerHTML = `
-                        <strong>Temperature:</strong> ${weatherResults.daily[3].temp.day} C`
+                        Temperature: ${weatherResults.daily[3].temp.day} C`
 
                         document.getElementById('dayFourHumidity').innerHTML = `
-                        <strong>Humidity:</strong> ${weatherResults.daily[3].humidity}%`
+                        Humidity: ${weatherResults.daily[3].humidity}%`
 
                         // Day 5's weather
                         document.getElementById('dayFiveIcon').innerHTML = `
                         <img src="https://openweathermap.org/img/wn/${weatherResults.daily[4].weather[0].icon}.png" alt="${weatherResults.daily[4].weather[0].description} weather icon.">`
 
                         document.getElementById('dayFiveTemp').innerHTML = `
-                        <strong>Temperature:</strong> ${weatherResults.daily[4].temp.day} C`
+                        Temperature: ${weatherResults.daily[4].temp.day} C`
 
                         document.getElementById('dayFiveHumidity').innerHTML = `
-                        <strong>Humidity:</strong> ${weatherResults.daily[4].humidity}%`
+                        Humidity: ${weatherResults.daily[4].humidity}%`
 
                     })
 
